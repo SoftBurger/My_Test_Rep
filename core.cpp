1024f0b3b159b21fe13f077459a3d5e1ccba72f8
SoftBurger <burgerITer@rambler.ru>
2022-11-29 19:57:30 +0300

    Add some code

--- core.cpp
+++ core.cpp
@@ -1,4 +1,16 @@
 class Core {
 public:
-  virtual void GetSome();
+  Core() {}
+  virtual void GetSome() {
+    GetComputation();
+  }
+};
+
+class SomeX {
+public:
+  int GetWidth() const;
+  int GetHeight() const;
+private:
+  int iWidth;
+  int iHeight;
 };
