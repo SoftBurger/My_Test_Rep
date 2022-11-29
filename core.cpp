3b28160134b7e8b9b42dfbfa419104b99ecf7745
SoftBurger <burgerITer@rambler.ru>
2022-11-29 20:02:09 +0300

    r

--- core.cpp
+++ core.cpp
@@ -8,9 +8,13 @@ public:
 
 class SomeX {
 public:
+  SomeX : iWidth{100}, iHeight{100} {}
   int GetWidth() const;
   int GetHeight() const;
 private:
   int iWidth;
   int iHeight;
+
+  // ...
+  float calc;
 };
